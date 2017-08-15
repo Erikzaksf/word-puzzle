727b1f42fa30377f9c31684b1bd02eae54c4c03b
Erik Zakrzewski and Kelsey Langlois <erik@>
2017-08-15 10:47:17 -0700

    Change js to display guess results in-page.

--- js/scripts.js
+++ js/scripts.js
@@ -17,16 +17,27 @@ $(document).ready(function () {
     }
     puzzleSentence = puzzleSentenceArray.join('');
     $("#sentence").hide();
+    $("#sentence-guess").show();
     $(".results").append("<p>" + puzzleSentence + "</p>");
   });
 
   $("#sentence-guess").submit(function(event) {
     event.preventDefault();
+    $("#wrong-answer").remove();
     var userGuess = $("#user-guess").val();
     if (userGuess === originalSentence) {
-      alert("Correct!");
+      $(".results").empty();
+      $(".results").append("<p>Correct!</p>");
+      $(".results").append("<p>Original Sentence: " + originalSentence + "</p>");
+      $(".results").addClass("alert alert-success");
     } else {
-      alert("Please Try Again.");
+      $("#user-guess").addClass("is-invalid");
+      $(".results").append("<p id='wrong-answer'>Please Try Again</p>");
+      $("#wrong-answer").addClass("alert alert-danger");
     }
+  });
+
+  $("#user-guess").focus(function() {
+      $("#user-guess").removeClass("is-invalid");
   });
 });
